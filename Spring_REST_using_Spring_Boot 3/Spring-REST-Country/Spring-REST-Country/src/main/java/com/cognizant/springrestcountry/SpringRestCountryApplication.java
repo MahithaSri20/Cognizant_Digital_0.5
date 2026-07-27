44b05366afb68944b700b1d3cb207d81f0bec93d
Mahitha Sri <mahithasri2006@gmail.com>
2026-07-27 21:18:35 +0530

    Completed REST Country Web Service HandsOn

---
+++ Spring_REST_using_Spring_Boot 3/Spring-REST-Country/Spring-REST-Country/src/main/java/com/cognizant/springrestcountry/SpringRestCountryApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.springrestcountry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestCountryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestCountryApplication.class, args);
+    }
+
+}
