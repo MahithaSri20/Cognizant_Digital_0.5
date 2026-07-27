1a0b38cf83b74007e55338d1d3d0690889a8321e
Mahitha Sri <mahithasri2006@gmail.com>
2026-07-27 23:05:13 +0530

    Completed REST Country Code service

---
+++ Spring_REST_using_Spring_Boot 3/Spring-REST-Country-Code/Spring-REST-Country-Code/src/main/java/com/cognizant/springrestcountrycode/service/CountryService.java
@@ -0,0 +1,43 @@
+package com.cognizant.springrestcountrycode.service;
+
+
+import com.cognizant.springrestcountrycode.model.Country;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class CountryService {
+
+
+    public Country getCountry(String code) {
+
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+
+        List<Country> countries =
+                (List<Country>) context.getBean("countryList");
+
+
+        for(Country country : countries) {
+
+
+            if(country.getCode().equalsIgnoreCase(code)) {
+
+                return country;
+
+            }
+
+        }
+
+
+        return null;
+
+    }
+
+}
---
+++ Spring_REST_using_Spring_Boot 3/Spring-REST-Country-Code/Spring-REST-Country-Code/src/test/java/com/cognizant/springrestcountrycode/SpringRestCountryCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.springrestcountrycode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestCountryCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
